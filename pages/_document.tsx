93c0c08715ff10589df501c9b50ca11703b8c57c
promobusca <matheus.vendas.afiliado@gmail.com>
2023-03-29 08:27:41 -0300

    correção meta viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          <meta name="theme-color" content="#ff6341" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/logos/logo-128x128.png" />
           <link rel="icon" href="/favicon.ico"></link>
-          <meta name="theme-color" content="#ff6341" />
-          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         </Head>
         <body className='scrollbar scrollbar-thumb-color2 scrollbar-track-color1 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default'>
           <Main />
