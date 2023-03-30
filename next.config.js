528f5a36056acd0e9282b4d994154f338d29368c
promobusca <matheus.vendas.afiliado@gmail.com>
2023-03-29 22:46:18 -0300

    correção CSP security

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const ContentSecurityPolicy = `
   object-src 'none';
   form-action 'self';
   script-src-elem 'self' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com;
-  script-src 'self' https: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com 'sha256-${hash.digest('base64')}';
+  script-src 'self' https: 'unsafe-eval' 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com 'sha256-${hash.digest('base64')}';
   style-src 'self' 'unsafe-inline' *.googletagmanager.com *.tagmanager.google.com *.fonts.googleapis.com https://fonts.googleapis.com;
   img-src 'self' data: blob: 'unsafe-inline' *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com;
   media-src *;
