d088ad21c8e19c59c64aa7f232a88a996bc4f35f
promobusca <matheus.vendas.afiliado@gmail.com>
2023-04-07 20:54:43 -0300

    correção de segurança

--- next.config.js
+++ next.config.js
@@ -17,16 +17,13 @@ const ContentSecurityPolicy = `
   object-src 'none';
   form-action 'self';
   script-src-elem 'self' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com;
-  script-src 'self' https: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com 'sha256-${hash.digest('base64')}';
+  script-src 'self' https: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic' *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com 'sha256-${hash.digest('base64')}' 'unsafe-eval';
   style-src 'self' 'unsafe-inline' *.googletagmanager.com *.tagmanager.google.com *.fonts.googleapis.com https://fonts.googleapis.com;
   img-src 'self' data: blob: 'unsafe-inline' *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com;
   media-src *;
   connect-src 'self' 'unsafe-inline' *.fonts.googleapis.com https://fonts.googleapis.com *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com vitals.vercel-insights.com;
   font-src 'self' 'unsafe-inline' https://fonts.gstatic.com;
-  trusted-types ${JSON.stringify({
-  default: ["'self'"],
-  script: ["'nonce-" + nonce + "'", "'strict-dynamic'"]
-}).replace(/"/g, "'")}
+  trusted-types ${JSON.stringify({ 'default': ["'self'"], 'script': ["'nonce-" + nonce + "'", "'strict-dynamic'"] })};
 `;
 
 const securityHeaders = [
