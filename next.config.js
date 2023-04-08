f6d47835ad6e21fb1c43894edb95f5e30a075e1a
promobusca <matheus.vendas.afiliado@gmail.com>
2023-04-08 00:19:38 -0300

    google analytics

--- next.config.js
+++ next.config.js
@@ -21,9 +21,9 @@ const hash = crypto.SHA256(script).toString(crypto.enc.Base64);
 
 // Define a política de segurança de conteúdo
 const ContentSecurityPolicy = `
-  default-src 'self' https://www.googletagmanager.com/gtag/js?id=G-FFC6EB1GNB;
+  default-src 'self' https://www.googletagmanager.com;
   script-src 'nonce-${nonce}' 'sha256-${hash}' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com https://www.google.com https://www.gstatic.com 'strict-dynamic' 'unsafe-inline';
-  script-src-elem 'self' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
+  script-src-elem 'self' 'nonce-${nonce}' 'sha256-${hash}' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com https://www.google.com https://www.gstatic.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' data: https://www.google-analytics.com;
   font-src 'self' https://fonts.gstatic.com;
