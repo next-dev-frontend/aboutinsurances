9fe97af49deaee3f7eae753bb409f25690d19d9b
promobusca <matheus.vendas.afiliado@gmail.com>
2023-04-09 23:54:55 -0300

    google analytics

--- next.config.js
+++ next.config.js
@@ -45,8 +45,8 @@ const gtmHash = crypto.createHash('sha256').update(gtmScript).digest('base64');
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com 'strict-dynamic' 'sha256-${gaHash}' 'sha256-${gtmHash}' 'nonce-${gaNonce}' 'nonce-${gtmNonce}';
-  script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com 'sha256-${gaHash}' 'sha256-${gtmHash}' 'nonce-${gaNonce}' 'nonce-${gtmNonce}';
+  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com 'strict-dynamic' 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}'; 
+  script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}';
   style-src 'self' 'unsafe-inline';
   object-src 'none';
   img-src * blob: data: https: http:;
