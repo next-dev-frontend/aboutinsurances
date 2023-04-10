a546212bd440ddbadbacb95e5ac603e0a4f3b65e
promobusca <matheus.vendas.afiliado@gmail.com>
2023-04-10 01:00:17 -0300

    google analytics

--- next.config.js
+++ next.config.js
@@ -58,8 +58,8 @@ const gtmHash = crypto.createHash('sha256').update(gtmScript).digest('base64');
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' https://aboutinsurances.vercel.app/_next/static/chunks/main-4d28554435b92d82.js https://www.googletagmanager.com https://www.google-analytics.com 'strict-dynamic' 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}' 'sha256-${mainHash}'}; 
-  script-src-elem 'self' https://aboutinsurances.vercel.app/_next/static/chunks/main-4d28554435b92d82.js https://www.googletagmanager.com https://www.google-analytics.com 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}' 'sha256-${mainHash}';
+  script-src 'self' https://aboutinsurances.vercel.app/_next/static/chunks/main-4d28554435b92d82.js https://www.googletagmanager.com https://www.google-analytics.com 'strict-dynamic' 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'nonce-${mainNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}' 'sha256-${mainHash}'}; 
+  script-src-elem 'self' https://aboutinsurances.vercel.app/_next/static/chunks/main-4d28554435b92d82.js https://www.googletagmanager.com https://www.google-analytics.com 'nonce-${gaNonce}' 'nonce-${gtmNonce}' 'nonce-${mainNonce}' 'sha256-${gaHash}' 'sha256-${gtmHash}' 'sha256-${mainHash}';
   style-src 'self' 'unsafe-inline';
   object-src 'none';
   img-src * blob: data: https: http:;
