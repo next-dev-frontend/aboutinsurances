07298cdaf091936716764c9ae6965705a06a3454
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-04-14 20:33:12 -0300

    correção images

--- next.config.js
+++ next.config.js
@@ -71,8 +71,9 @@ module.exports = withImages(withPWA({
   distDir: '.next',
 
   images: {
+    formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [16, 32, 36, 48, 64, 72, 96, 128, 144, 192, 256, 384, 512],
   },
 
   async headers() {
