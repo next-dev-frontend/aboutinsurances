da29ccb408aa86f1ce237201049517fe6baedeac
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-04-14 22:44:38 -0300

    correção images

--- components/BgParallax2.js
+++ components/BgParallax2.js
@@ -1,16 +1,8 @@
-import Image from "next/image"
-
 function BgParallax2() {
   return (
     <section>
-      <div className="py-20 px-8 bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
-        <div className="relative select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
-        <Image
-          src="/backgrounds/bg6-insurances.webp"
-          alt="background insurances"
-          layout="fill"
-          objectFit="cover"
-        />
+      <div className="bg-[url('/backgrounds/bg6-insurances.webp')] py-20 px-8 bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
+        <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
           <h2 className="sticky bg-light-green bg-opacity-80 p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
             "Security is not the end goal. It's a starting point."
           </h2>
