04c5e0ea05809fc5ff2d3e3b8dfe1d61d9aca84f
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-04-16 22:52:13 -0300

    google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useRouter } from "next/router";
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
 import SEO from '../next-seo-config';
-import { initGA, logPageView } from '../ga';
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import * as gtag from '../lib/gtag';
+import Analytics from '../components/Analytics';
 
 function MyApp({ Component, pageProps }) {
 
-    //google analytics
-    useEffect(() => {
-      if (!window.GA_INITIALIZED) {
-        initGA();
-        window.GA_INITIALIZED = true;
-      }
-      logPageView();
-    }, []);
+ // google tags  
+ const router = useRouter();
+
+ useEffect(() => {
+   const handleRouteChange = (url) => {
+     gtag.pageview(url);
+   };
+   //quando o componente é montado, assina as 
+   //alterações do roteador e registras as visualizações
+   router.events.on('routeChangeComplete', handleRouteChange);
+
+   // se o componente estiver desmontado, cancela
+   // a assinatura do evento com o método `off`
+   return () => {
+     router.events.off('routeChangeComplete', handleRouteChange);
+   };
+ }, [router.events]);
 
     //registrar service-worker
     useEffect(() => {
@@ -41,6 +52,7 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
