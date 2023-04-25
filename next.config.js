70e6b6c0501cea0f18be3a7db1f9627b32fb814f
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-04-24 22:21:14 -0300

    segurança

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ const nonce = crypto.randomBytes(16).toString('base64');
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ 'strict-dynamic' 'unsafe-inline';
-  script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ 'strict-dynamic' 'unsafe-inline';
-  style-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ 'unsafe-hashes';
+  script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ 'unsafe-inline';
+  style-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ 'unsafe-inline';
   img-src 'self' https://www.googletagmanager.com data:;
   connect-src 'self' https://www.googletagmanager.com https://www.googletagmanager.com/gtag/ vitals.vercel-insights.com;
   font-src 'self';
