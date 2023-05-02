63438ecaccf5bdfc3582fc44c4671c8b1c1f1d3f
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-02 19:36:08 -0300

    analytics 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,11 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/logos/logo-128x128.png" />
           <link rel="icon" href="/favicon.ico"></link>
           <meta name="theme-color" content="#ff6341" />
-          <GoogleAnalytics />
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          />
+
         </Head>
         <body className="scrollbar scrollbar-thumb-color2 scrollbar-track-color1 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
           <Main />
