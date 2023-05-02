2282ff4334e5dce3a2382885abaaca076f5e5ee9
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-02 19:24:22 -0300

    analytics 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Script from 'next/script';
+import dynamic from 'next/dynamic';
+const GoogleAnalytics = dynamic(() => import('../components/GoogleAnalyticsScript'))
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -15,20 +17,7 @@ class MyDocument extends Document {
             strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
           />
-          <Script
-            id="ga-tracking"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-            }}
-          />
+          <GoogleAnalytics />
         </Head>
         <body className="scrollbar scrollbar-thumb-color2 scrollbar-track-color1 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
           <Main />
