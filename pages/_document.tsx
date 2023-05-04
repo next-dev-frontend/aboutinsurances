bce6fcaf3cbaa68f913e6e26082d11f9dfdfd7bb
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-03 21:10:06 -0300

    analytics 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
+                page_path: location.pathname,
               });
             `,
             }}
