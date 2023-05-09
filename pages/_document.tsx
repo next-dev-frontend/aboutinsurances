b411afc297966ac11d2b1e97318e31a21ce5fafe
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-08 21:46:10 -0300

    analytics 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Analytics from '../components/Analytics';
+import dynamic from 'next/dynamic'
+const Analytics = dynamic(() => import('../components/Analytics'))
 
 class MyDocument extends Document {
   render() {
