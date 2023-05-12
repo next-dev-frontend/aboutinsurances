595a10f2d7dc63358426f79779127184373b3af8
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-12 20:51:05 -0300

    gerar sitemap.xml

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,14 +1,15 @@
 const sitemap = require('nextjs-sitemap-generator');
+
 const targetDirectory = 'public';
 
 sitemap({
   baseUrl: 'https://aboutinsurances.vercel.app',
-  pagesDirectory: __dirname + '\\pages',
+  pagesDirectory: __dirname + '/pages',
   ignoreIndexFiles: true,
   targetDirectory: targetDirectory,
-  nextConfigPath: __dirname + "\\next.config.js",
+  nextConfigPath: __dirname + "/next.config.js",
   pagesConfig: {
-    '/pages': {
+    '/': {
       priority: '1',
       changefreq: 'daily',
     },
