a6013da7c10a72a2130ea604467b18f10a4d6542
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-12 22:48:35 -0300

    gerar sitemap.xml

--- components/Analytics.js
+++ components/Analytics.js
@@ -3,13 +3,13 @@ import ls from 'local-storage';
 
 const CookiePopup = ({ onClose }) => (
   <>
-    <div className='modal bg-color2 border-4 rounded border-color1'>
+    <div className='modal bg-white border-4 rounded border-color1'>
       <p className='text-center font-bold'>This site uses cookies</p>
       <br />
       <p>This website uses cookies to improve your user experience. By continuing to browse the site, you agree to the use of cookies.</p>
       <br />
       <div className="flex justify-center">
-        <button className='bg-white border-2 rounded border-color1 text-lg p-4 cursor-pointer' onClick={onClose}>Accepted</button>
+        <button className='bg-color2 border-2 rounded border-color1 text-lg p-4 cursor-pointer' onClick={onClose}>Accepted</button>
       </div>
       <span className='absolute top-0 right-0 mt-4 mr-4 text-xl cursor-pointer' onClick={onClose} >
         &times;
@@ -19,9 +19,11 @@ const CookiePopup = ({ onClose }) => (
 );
 
 const App = () => {
+  const [isClient, setIsClient] = useState(false);
   const [showCookiePopup, setShowCookiePopup] = useState(!ls.get('cookieAccepted'));
 
   useEffect(() => {
+    setIsClient(true);
     if (!showCookiePopup) {
       // Carrega o script do Analytics quando o usuário aceita os cookies
       const script = document.createElement('script');
@@ -38,7 +40,7 @@ const App = () => {
 
   return (
     <React.Fragment>
-      {showCookiePopup && <CookiePopup onClose={handleCloseCookiePopup} />}
+      {isClient && showCookiePopup && <CookiePopup onClose={handleCloseCookiePopup} />}
       {/* Renderiza o Analytics somente após o usuário aceitar os cookies */}
     </React.Fragment>
   );
