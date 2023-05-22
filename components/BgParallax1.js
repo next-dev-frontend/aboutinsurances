78d7bfb0a5146c63c39498efa6b6deb87c759f1f
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-21 21:19:19 -0300

    correção unsafe-inline em style-src

--- components/BgParallax1.js
+++ components/BgParallax1.js
@@ -1,15 +1,48 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
 function BgParallax1() {
+  const [offsetY, setOffsetY] = useState(0);
+
+  const handleScroll = () => {
+    setOffsetY(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <section>
-      <div className="bg-[url('/backgrounds/bg1-insurances.webp')] py-20 px-8 bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
-        <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
-          <h1 className="sticky bg-light-green bg-opacity-80 p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
-            Security doesn't come with luck. You have to be prepared.
-          </h1>
-          <div className="sticky bg-white h-1 flex flex-col w-full"></div>
+    <div className="relative bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
+      <div className="absolute inset-0 z-[-1]">
+        <div
+          className="w-screen h-screen"
+          style={{
+            transform: `translateY(${offsetY * 0.5}px)`
+          }}
+        >
+          <Image
+            src="/backgrounds/bg1-insurances.webp"
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
         </div>
       </div>
-    </section>
-  )
-}
-export default BgParallax1
+      <div className="absolute inset-0"></div>
+      <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
+        <h1 className="sticky bg-color1 bg-opacity-80 p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
+          Security doesn't come with luck. You have to be prepared.
+        </h1>
+        <div className="sticky bg-white h-1 flex flex-col w-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BgParallax1;
--- components/BgParallax2.js
+++ components/BgParallax2.js
@@ -1,16 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+
 function BgParallax2() {
   return (
-    <section>
-      <div className="bg-[url('/backgrounds/bg6-insurances.webp')] py-20 px-8 bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
-        <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
-          <h2 className="sticky bg-light-green bg-opacity-80 p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
-            "Security is not the end goal. It's a starting point."
-          </h2>
-          <p className="text-3xl">- Wendy Kopp</p>
-          <div className="sticky bg-white h-1 flex flex-col w-full"></div>
+    <div className="relative bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-full max-w-full h-96 md:h-screen">
+      <div className="absolute inset-0 z-[-1]">
+        <div>
+          <Image
+            src="/backgrounds/bg1-insurances.webp"
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
       </div>
-    </section>
-  )
-}
-export default BgParallax2
+      <div className="absolute inset-0"></div>
+      <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
+        <h2 className="sticky bg-color1 bg-opacity-80 p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
+          Security doesn't come with luck. You have to be prepared.
+        </h2>
+        <div className="sticky bg-white h-1 flex flex-col w-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BgParallax2;
