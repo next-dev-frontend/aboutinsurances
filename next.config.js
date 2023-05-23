633d89b93b83add92d27e60f7ea5ce9e1aafb69c
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-05-23 01:09:19 -0300

    correção unsafe-inline em style-src

--- next.config.js
+++ next.config.js
@@ -52,7 +52,7 @@ const securityHeaders = [
   },
   {
     key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload'
+    value: 'max-age=3571000; includeSubDomains; preload'
   },
   {
     key: 'X-XSS-Protection',
