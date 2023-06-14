1bcabcb797e1b7989433dc8877880c74eac539ad
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-13 23:03:57 -0300

    bg parallax

--- components/BgParallax1.js
+++ components/BgParallax1.js
@@ -1,9 +1,10 @@
 function BgParallax1() {
   return (
     <div>
-      <section className="relative bg-center flex items-center justify-center bg-fixed bg-cover bg-no-repeat w-screen h-96 md:h-max">
-        <div className="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat bg-fixed" style={{ backgroundImage: 'url(/backgrounds/bg1-insurances.webp)', alt: 'Background Image' }} />
-        <div className="select-none sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
+      <section className="select-none relative bg-center flex items-center justify-center w-screen h-96 md:h-full">
+        <div className="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat bg-fixed" style={{ backgroundImage: 'url(/backgrounds/bg1-insurances.webp)' }} />
+        <span className="sr-only">Image Background</span>
+        <div className="sticky h-max max-w-3xl text-white bg-gray-900 bg-opacity-30">
           <h2 className="sticky p-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-bold text-center md:text-left">
             "Security is not the end goal. It's a starting point."
           </h2>
