b4a380833fb899de1b15f1b2f3dff7a4baddb4b3
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-21 21:12:28 -0300

    meta tags on head

--- next-seo-config.js
+++ next-seo-config.js
@@ -6,11 +6,11 @@ export default {
     site_name: 'Website About Insurances',
     images: [
       {
-        url: 'https:/aboutinsurances.vercel.app/backgrounds/bg-insurances.webp',
+        url: 'https:/aboutinsurances.vercel.app/backgrounds/bg-insurances.jpeg',
         alt: 'logo about insurances',
         width: 380,
         height: 380,
-        type: 'image/webp'
+        type: 'image/jpeg'
       }
     ]
   }
