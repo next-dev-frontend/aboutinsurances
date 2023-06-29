1ec8ac2c78bed4338ab11d07fcc364e3a3132714
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-29 20:32:25 -0300

    google adsense

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nonceScriptSrc = crypto.randomBytes(16).toString('base64');
 
 const ContentSecurityPolicy = `
 base-uri 'self';  
-connect-src 'self' 'unsafe-inline' webpack://* *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com https://*.analytics.google.com https://www.analytics.google.com https://analytics.google.com https://stats.g.doubleclick.net/g/collect https://www.google.com.br/ads/ga-audiences vitals.vercel-insights.com;
+connect-src 'self' 'unsafe-inline' webpack://* *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com https://*.analytics.google.com https://www.analytics.google.com https://analytics.google.com https://stats.g.doubleclick.net/g/collect https://www.google.com.br/ads/ga-audiences https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js vitals.vercel-insights.com;
 default-src 'self' https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js;
 font-src 'self';
 form-action 'self';  
