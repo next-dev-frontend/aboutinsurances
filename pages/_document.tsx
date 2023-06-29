8c4a5214c888c2e1f24928d4dd18c362477141a3
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-28 23:52:37 -0300

    google adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -8,6 +9,15 @@ class MyDocument extends Document {
         <Head>
           <meta name="google-site-verification" content="rRKlKFWnlhU_kUGxe4fgENDoEIIcYSSc1DbdpRMn1kY" />
           <meta name="theme-color" content="#ff6341" />
+          <Script
+            id="Absence-banner"
+            strategy="afterInteractive"
+            onError={(e) => {
+              console.error('Script failed to load', e);
+            }}
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+            crossOrigin="anonymous"
+          />
         </Head>
         <body className="scrollbar scrollbar-thumb-color1 scrollbar-track-color2 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
           <Main />
