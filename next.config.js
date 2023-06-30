774badcc985e22dca1bd5ed036d8bd68c99215bf
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-29 22:14:38 -0300

    google adsense

--- next.config.js
+++ next.config.js
@@ -18,16 +18,16 @@ const nonceScriptSrc = crypto.randomBytes(16).toString('base64');
 
 const ContentSecurityPolicy = `
 base-uri 'self';  
-connect-src 'self' 'unsafe-inline' webpack://* *.gstatic.com *.googletagmanager.com *.tagmanager.google.com *.google-analytics.com https://*.analytics.google.com https://www.analytics.google.com https://analytics.google.com https://stats.g.doubleclick.net/g/collect https://www.google.com.br/ads/ga-audiences pagead2.googlesyndication.com vitals.vercel-insights.com;
+connect-src 'self' 'unsafe-inline' webpack://* *.googleadservices.com *.googletagmanager.com *.googletagservices.com *.googlesyndication.com *.google-analytics.com *.googleapis.com *.ggpht.com *.google.com *.google.co.uk *.gstatic.com *.doubleclick.net vitals.vercel-insights.com;
 default-src 'none';
 font-src 'self';
 form-action 'self';  
 frame-ancestors 'self';
-frame-src 'self' https://www.google.com/ googleads.g.doubleclick.net tpc.googlesyndication.com;
-img-src 'self' data: blob: https: 'unsafe-inline' https://*.gstatic.com https://*.google.com https://*.googletagmanager.com https://www.googletagmanager.com/gtag/js https://*.tagmanager.google.com https://*.google-analytics.com https://*.google.com.br/ads/ storage.googleapis.com pagead2.googlesyndication.com;  
+frame-src 'self' *.googleadservices.com *.googletagmanager.com *.googletagservices.com *.googlesyndication.com *.google-analytics.com *.googleapis.com *.ggpht.com *.google.com *.google.co.uk *.gstatic.com *.doubleclick.net;
+img-src 'self' data: blob: https: 'unsafe-inline' *.googleadservices.com *.googletagmanager.com *.googletagservices.com *.googlesyndication.com *.google-analytics.com *.googleapis.com *.ggpht.com *.google.com *.google.co.uk *.gstatic.com *.doubleclick.net;  
 manifest-src 'self';
 object-src 'none';
-script-src 'self' https: 'nonce-${nonceScriptSrc}' 'unsafe-inline' 'strict-dynamic' ${isProduction ? "" : "'unsafe-eval'"} https://aboutinsurances.vercel.app/* https://*.googletagmanager.com https://*.tagmanager.google.com https://*.google-analytics.com https://www.googletagmanager.com/gtag/js;
+script-src 'self' https: 'nonce-${nonceScriptSrc}' 'unsafe-inline' 'strict-dynamic' ${isProduction ? "" : "'unsafe-eval'"} https://aboutinsurances.vercel.app/* *.googleadservices.com *.googletagmanager.com *.googletagservices.com *.googlesyndication.com *.google-analytics.com *.googleapis.com *.ggpht.com *.google.com *.google.co.uk *.gstatic.com *.doubleclick.net;
 script-src-elem 'self' 'unsafe-inline' https: *.googleadservices.com *.googletagmanager.com *.googletagservices.com *.googlesyndication.com *.google-analytics.com *.googleapis.com *.ggpht.com *.google.com *.google.co.uk *.gstatic.com *.doubleclick.net;
 style-src 'self' 'unsafe-inline';
 `;
