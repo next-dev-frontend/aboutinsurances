cd401226b28882f7dc1345c30123a5d04b045813
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-06-29 22:29:05 -0300

    google adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import AdSense from "../components/AdSense";
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -9,11 +10,12 @@ class MyDocument extends Document {
         <Head>
           <meta name="google-site-verification" content="rRKlKFWnlhU_kUGxe4fgENDoEIIcYSSc1DbdpRMn1kY" />
           <meta name="theme-color" content="#ff6341" />
-          <script
+          <Script
+            id="Adsense-id"
             data-ad-client="ca-pub-4253472436511726"
-            async
+            strategy="beforeInteractive"
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script>
+          />
         </Head>
         <body className="scrollbar scrollbar-thumb-color1 scrollbar-track-color2 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
           <AdSense adSlot="1234567890" />
